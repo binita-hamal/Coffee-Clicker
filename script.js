d1ea40fd5e33454573ca7bff493db27f87e9111f
winiiee <binita.hamal7@gmail.com>
2025-07-24 09:46:22 +0545

    coffee-clicker-completed

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 /* eslint-disable no-alert */
+//store to localstorage
+
+function storeToLocalStorage(data){
+  localStorage.setItem('CoffeeCount', JSON.stringify(data));//convert object into string as localstorage store only strings datatype.
+
+}
+
+function loadPreviousValue() {
+  const saved = localStorage.getItem('CoffeeCount');
+  if (saved) {
+    return JSON.parse(saved); // Convert back to object
+  }
+  return null;
+}
 
 /**************
  *   SLICE 1
@@ -6,10 +20,18 @@
 
 function updateCoffeeView(coffeeQty) {
   // your code here
+
+  const coffeeCounter = document.getElementById("coffee_counter");
+  coffeeCounter.innerText = coffeeQty;
+  renderProducers(data);
 }
 
 function clickCoffee(data) {
+  //value of coffeee is coming
   // your code here
+  data.coffee++;
+  updateCoffeeView(data.coffee);
+  storeToLocalStorage(data)
 }
 
 /**************
@@ -18,20 +40,41 @@ function clickCoffee(data) {
 
 function unlockProducers(producers, coffeeCount) {
   // your code here
+  for (let producer of producers) {
+    if (coffeeCount >= producer.price / 2) {
+      producer.unlocked = true;
+    }
+  }
 }
 
 function getUnlockedProducers(data) {
   // your code here
+  return data.producers.filter((d) => {
+    return d.unlocked === true;
+  });
 }
 
 function makeDisplayNameFromId(id) {
   // your code here
+  let arr = id.split("_");
+
+  let titleCase = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    titleCase += arr[i][0].toUpperCase() + arr[i].slice(1);
+
+    if (i < arr.length - 1) {
+      titleCase += " ";
+    }
+  }
+
+  return titleCase;
 }
 
 // You shouldn't need to edit this function-- its tests should pass once you've written makeDisplayNameFromId
 function makeProducerDiv(producer) {
-  const containerDiv = document.createElement('div');
-  containerDiv.className = 'producer';
+  const containerDiv = document.createElement("div");
+  containerDiv.className = "producer";
   const displayName = makeDisplayNameFromId(producer.id);
   const currentCost = producer.price;
   const html = `
@@ -51,10 +94,25 @@ function makeProducerDiv(producer) {
 
 function deleteAllChildNodes(parent) {
   // your code here
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 }
 
 function renderProducers(data) {
   // your code here
+  const producerContainer = document.getElementById("producer_container");
+
+  unlockProducers(data.producers, data.coffee);
+  //get list of unlocked producers
+  let listOfUnlockedProducers = getUnlockedProducers(data);
+  console.log("Unlocked producers:", listOfUnlockedProducers);
+
+  deleteAllChildNodes(producerContainer);
+
+  for (let list of listOfUnlockedProducers) {
+    producerContainer.append(makeProducerDiv(list));
+  }
 }
 
 /**************
@@ -63,31 +121,87 @@ function renderProducers(data) {
 
 function getProducerById(data, producerId) {
   // your code here
+  for (let producer of data.producers) {
+    if (producer.id === producerId) {
+      return producer;
+    }
+  }
 }
 
 function canAffordProducer(data, producerId) {
   // your code here
+
+  for (let producer of data.producers) {
+    if (producer.id === producerId) {
+      return data.coffee >= producer.price;
+    }
+  }
 }
 
 function updateCPSView(cps) {
   // your code here
+
+  const cpsIndicator = document.getElementById("cps");
+  cpsIndicator.innerText = cps;
 }
 
 function updatePrice(oldPrice) {
   // your code here
+  return Math.floor((oldPrice * 125) / 100);
 }
 
 function attemptToBuyProducer(data, producerId) {
   // your code here
+
+
+  let listOfCanAfford = canAffordProducer(data, producerId);
+  if (listOfCanAfford) {
+    //true;
+
+    let producer = getProducerById(data, producerId);
+
+    producer.qty++;
+    data.coffee = data.coffee - producer.price;
+
+    producer.price = updatePrice(producer.price);
+    data.totalCPS = data.totalCPS + producer.cps;
+    storeToLocalStorage(data);
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function buyButtonClick(event, data) {
   // your code here
+ 
+  if (event.target.tagName !== "BUTTON") return;
+  const producerId = event.target.id.slice(4); 
+
+
+  if(attemptToBuyProducer(data, producerId)){
+    updateCoffeeView(data.coffee);
+    updateCPSView(data.totalCPS);
+    renderProducers(data);
+    storeToLocalStorage(data);
+
+  } 
+  else {
+    window.alert("Not enough coffee!");
+  }
 }
 
+
+
 function tick(data) {
-  // your code here
+  data.coffee += data.totalCPS;
+  updateCoffeeView(data.coffee);
+  renderProducers(data);
+  storeToLocalStorage(data);
 }
+
+
+
 
 /*************************
  *  Start your engines!
@@ -103,19 +217,24 @@ function tick(data) {
 
 // How does this check work? Node gives us access to a global variable /// called `process`, but this variable is undefined in the browser. So,
 // we can see if we're in node by checking to see if `process` exists.
-if (typeof process === 'undefined') {
+if (typeof process === "undefined") {
   // Get starting data from the window object
   // (This comes from data.js)
-  const data = window.data;
+  const data = loadPreviousValue() ||  window.data;
+if(loadPreviousValue()){
+  updateCoffeeView(data.coffee) //0
+  updateCPSView(data.totalCPS) //0
+  renderProducers(data)
+}
 
   // Add an event listener to the giant coffee emoji
-  const bigCoffee = document.getElementById('big_coffee');
-  bigCoffee.addEventListener('click', () => clickCoffee(data));
+  const bigCoffee = document.getElementById("big_coffee");
+  bigCoffee.addEventListener("click", () => clickCoffee(data));
 
   // Add an event listener to the container that holds all of the producers
   // Pass in the browser event and our data object to the event listener
-  const producerContainer = document.getElementById('producer_container');
-  producerContainer.addEventListener('click', event => {
+  const producerContainer = document.getElementById("producer_container");
+  producerContainer.addEventListener("click", (event) => {
     buyButtonClick(event, data);
   });
 
@@ -142,6 +261,6 @@ else if (process) {
     updatePrice,
     attemptToBuyProducer,
     buyButtonClick,
-    tick
+    tick,
   };
 }
